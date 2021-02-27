1340c4277d866a8020e83b0695de0956839a3277
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-02-26 18:00:20 -0600

    numbers appear on screen when clicked
    Built out const and functions in JS

---
+++ assets/script.js
@@ -0,0 +1,47 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+    keys.addEventListener('click', (event) => {
+        const {target} = event;
+        // Check if clicked element is a button
+        if (!target.matches('button')) {
+            return;
+        }
+
+        if (target.classList.contains('operator')) {
+            console.log('operator', target.value);
+            return;
+        }
+
+        if (target.classList.contains('decimal')) {
+            console.log('decimal', target.value);
+            return;
+        }
+
+        if (target.classList.contains('all-clear')) {
+            console.log('clear', target.value);
+            return;
+        }
+
+        inputDigit(target.value);
+        updateDisplay();
+
+    });
+
+    function inputDigit(digit) {
+        const {displayValue} = calculator;
+
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
